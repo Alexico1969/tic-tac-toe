6e339e8327bee3855d70c9cd07b167b41d2875fa
Alex van Winkel <34400264+Alexico1969@users.noreply.github.com>
2025-02-06 14:09:50 -0500

    initial init

---
+++ ttt_01.py
@@ -0,0 +1,10 @@
+# tic-tac-toe game with 2 players
+# 3x3 board
+# player 1 is X
+# player 2 is O
+# player 1 starts the game
+# players take turns to place their mark on the board
+# first player to get 3 marks in a row, column or diagonal wins
+# if all 9 squares are filled and no player has 3 marks in a row, the game is a draw
+
+
